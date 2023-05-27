c2832e9cbde339a4d83351e31a834aea89aedb6d
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-28 02:19:01 +0300

    import icons and images

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png'
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 
 function Intro() {
@@ -36,7 +43,18 @@ I'm excited to put my knowledge in use and continue to develop my abilities in a
             </div>
         </div>
         <div className="i-right">
-            i AM RIGHT SIDE
+            <img src={Vector1} alt="" />
+            <img src={Vector2} alt="" />
+            <img src={boy} alt="" />
+            <img src={glassesimoji} alt="" />
+            
+            <div style={{top: '-4%', left: '68%'}}>
+                <FloatingDiv image={crown} txt1='Web' txt2='Developer'/>
+            </div>
+
+            <div style={{top: '18rem', left: '0rem'}}>
+                <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award'/>
+            </div>
         </div>
     </div>  )
 }
