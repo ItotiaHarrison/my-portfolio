10c59289f4ff8e93aa4de5d278c34d99bfbeee4d
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-28 23:15:42 +0300

    create experience section

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Experience.css'
+
+function Experience() {
+  return (
+    <div className="experience">
+        <div className="achievement">
+            <div className="circle">2+</div>
+            <span>years </span>
+            <span>experience </span>
+        </div>
+        <div className="achievement">
+            <div className="circle">2+</div>
+            <span>completed </span>
+            <span>projects </span>
+        </div>
+        <div className="achievement">
+            <div className="circle">2+</div>
+            <span>companies </span>
+            <span>worked for </span>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
