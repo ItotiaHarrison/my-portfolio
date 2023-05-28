ac6906cbb17e4f60f6ab8d4d04b9bed03bbd4844
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-28 22:41:27 +0300

    create the services cards

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Services.css'
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import Card from '../Card/Card';
+
+function Services() {
+    return (
+        <div className="services">
+            <div className="awesome">
+                <span>My Awesome</span>
+                <span>services</span>
+                <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore atque soluta
+                    <br />
+                    autem blanditiis amet, quis id numquam. Voluptas veritatis quos at quibusdam</span>
+
+                    <button className="button s-button">Download CV</button>
+                    <div className="blur s-blur" style={{background: 'ABF1FF94'}}></div>
+            </div>
+
+            <div className="cards">
+                <div style={{left: '14rem'}}>
+                    <Card emoji={HeartEmoji} heading={'Design'} detail={'Figma, Sketch, Photoshop, Adobe, Adobe xd'}/>
+                </div>
+
+                <div style={{top: '12rem', left: '-4rem'}}>
+                    <Card emoji={Glasses} heading={'Developer'} detail={'Html, CSS, Javascript, React'}/>
+                </div>
+
+                <div style={{top: '19rem', left: '12rem'}}>
+                    <Card emoji={Humble} heading={'UI/UX'} detail={'Html, CSS, Javascript, React'}/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Services
