894f7ed77d891946ae6898b7b4f17a0382b100b0
Harrison Itotia <harrisonitotia@gmail.com>
2023-05-29 17:02:23 +0300

    Create the footer, install react unicons iconscout

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Footer.css'
+import Wave from "../../img/wave.png";
+import Twitter from "@iconscout/react-unicons/icons/uil-twitter";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>itotiacodes@gmail.com</span>
+        <div className="f-icons">
+          <Twitter color="white" size={"3rem"} />
+          <Linkedin color="white" size={"3rem"} />
+          <Gitub color="white" size={"3rem"} />
+        </div>
+      </div>
+    </div>
+  )
+}
